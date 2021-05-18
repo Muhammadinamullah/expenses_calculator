f7a536b83cb4eac436b18f7d34b6f64a5f5920bc
inamullah <inamullah.oraxtech@gmail.com>
2021-05-18 19:12:03 +0500

    putted some styling to the container

--- lib/main.dart
+++ lib/main.dart
@@ -43,6 +43,10 @@ class MyHomePage extends StatelessWidget {
             return Card(
               child: Row(children: <Widget>[
                 Container(
+                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black, width: 2)),
+                  padding: EdgeInsets.all(10),
                   child: Text(tx.amount.toString()),
                 ),
                 Column(
